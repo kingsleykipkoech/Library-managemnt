adcc7115b2efcc19e347e95a2a9bc69420cec44a
kingsleykipkoech <kingsleykoech@gmail.com>
2026-02-17 00:10:59 +0100

    feat: added filters and fetches to Loan

--- library_management/library_management/doctype/loan/loan.js
+++ library_management/library_management/doctype/loan/loan.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2026, none and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Loan", {
-// 	refresh(frm) {
+frappe.ui.form.on("Loan", {
+    setup(frm) {
+        // Filter the 'book' link field to only show Available books
+        frm.set_query("book", function () {
+            return {
+                filters: {
+                    "status": "Available"
+                }
+            };
+        });
+    },
 
-// 	},
-// });
+    onload(frm) {
+        // frm.add_fetch("link_fieldname", "source_fieldname", "target_fieldname")
+        frm.add_fetch("applicant", "first_name", "full_name");
+    }
+});
