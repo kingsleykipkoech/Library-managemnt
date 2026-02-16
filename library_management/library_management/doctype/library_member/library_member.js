106ca8fe5f3955fc6ebe55e37f543a72e370c574
kingsleykipkoech <kingsleykoech@gmail.com>
2026-02-17 00:09:14 +0100

    feat: added welcome message client script

--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2026, none and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Library Member", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Library Member", {
+    refresh(frm) {
+        // Check if the member has a first name
+        if (frm.doc.first_name) {
+            frappe.msgprint("Welcome to the library, " + frm.doc.first_name + "!");
+        }
+    },
+});
